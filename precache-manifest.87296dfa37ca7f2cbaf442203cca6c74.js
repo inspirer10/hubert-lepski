f839b8d04f2637046cbb33235069aa3ff77d70ab
Hubert Łepski <hubertlepsky@gmail.com>
2022-09-01 19:45:13 +0200

    Updates

---
+++ precache-manifest.87296dfa37ca7f2cbaf442203cca6c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94039c2e4a5f400c659a",
+    "url": "/hubert-lepski/static/css/main.dc25bfc4.chunk.css"
+  },
+  {
+    "revision": "94039c2e4a5f400c659a",
+    "url": "/hubert-lepski/static/js/main.94039c2e.chunk.js"
+  },
+  {
+    "revision": "a322975c65224bc82c97",
+    "url": "/hubert-lepski/static/js/1.a322975c.chunk.js"
+  },
+  {
+    "revision": "6b28214e3cd7ea69eceb",
+    "url": "/hubert-lepski/static/js/2.6b28214e.chunk.js"
+  },
+  {
+    "revision": "d969fa28126ef096fd8c",
+    "url": "/hubert-lepski/static/js/runtime~main.d969fa28.js"
+  },
+  {
+    "revision": "729e331cca5c30911bcf65d170e17524",
+    "url": "/hubert-lepski/index.html"
+  }
+];
